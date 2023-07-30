d75bba0ccc05922bd8b8e9643d8836145be15e72
DiegoEstela <die.estela@gmail.com>
2023-07-30 17:40:39 +0200

    conditionals

---
+++ 3-semana/conditional.py
@@ -0,0 +1,29 @@
+a = 195
+b = 30
+c = 400
+
+if a > b and c > a:
+    print("Ambas condiciones son verdaderas ")
+
+d = 195
+f = 30
+g = 400
+
+if d > f or g > a:
+    print("Una de las condiciones es verdadera ")
+
+x = 10
+if not x > 15:
+    print("false")
+
+j = 150
+k = 330
+
+print("j") if j > k else print("=") if j == k else print("k")
+
+numero = 24
+
+if numero > 36:
+    print("el numero es grande")
+else:
+    print("el numero es pequeño")
