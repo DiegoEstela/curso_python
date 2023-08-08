d776863f7998d93b9cff895a226b664bb75c6b43
DiegoEstela <die.estela@gmail.com>
2023-08-09 00:53:41 +0200

    other challenge

--- 4-semana/challenge.py
+++ 4-semana/challenge.py
@@ -11,23 +11,26 @@ print("muy bien ya saliste")
 num1 = int(input("Ingrese primer numero a operar: "))
 num2 = int(input("Ingrese segundo numero a operar: "))
 
-print("""
-menu:
-1. Sumar
-2. Restar
-3. Multiplicar
-4. Salir
-""")
+flag = True
+while flag:
+    print("""
+    menu:
+    1. Sumar
+    2. Restar
+    3. Multiplicar
+    4. Salir
+    """)
 
-option = input("Ingrese opcion del menu que desea ejecutar: ")
+    option = input("Ingrese opcion del menu que desea ejecutar: ")
 
-if option == '1':
-    print(f'la suma de {num1} y {num2} es: {num1 + num2}')
-elif option == '2':
-    print(f'la resta de {num1} y {num2} es: {num1 - num2}')
-elif option == '3':
-    print(f'la multiplicacion de {num1} y {num2} es: {num1 * num2}')
-elif option == '4':
-    pass
-else:
-    print("Ingrese una opcion correcta!")
+    if option == '1':
+        print(f'la suma de {num1} y {num2} es: {num1 + num2}')
+    elif option == '2':
+        print(f'la resta de {num1} y {num2} es: {num1 - num2}')
+    elif option == '3':
+        print(f'la multiplicacion de {num1} y {num2} es: {num1 * num2}')
+    elif option == '4':
+        print("Volve a operar cuando gustes")
+        break
+    else:
+        print("Ingrese una opcion correcta!")
