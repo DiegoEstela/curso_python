4c201645777e38bc8e7fa8e523c32cf6dd72d9fb
DiegoEstela <die.estela@gmail.com>
2023-08-19 16:17:54 +0200

    range

--- 4-semana/for.py
+++ 4-semana/for.py
@@ -1,5 +1,14 @@
-numeros = [0, 1, 2, 3, 4, 5]
+""" numeros = [0, 1, 2, 3, 4, 5]
 for indice, numero in enumerate(numeros):
     numeros[indice] *= 5
 
-print(numeros)
+print(numeros) """
+
+""" for numero in range(10):
+    print(numero)
+ """
+""" for numero in range(5, 10):
+    print(numero)
+ """
+for numero in range(0, 10, 3):
+    print(numero)
