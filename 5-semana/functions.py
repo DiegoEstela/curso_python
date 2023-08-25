8613c01f10cad38b899be8ae900df8e6e5e1abd4
DiegoEstela <die.estela@gmail.com>
2023-08-25 20:29:37 +0200

    errors

---
+++ 5-semana/functions.py
@@ -0,0 +1,7 @@
+def saludar_por_nombre():
+    nombre = input("Escribe tu nombre: ")
+    return f"Hola {nombre}!.Bienvenido al curso de Python!"
+
+
+hola = saludar_por_nombre()
+print(hola)
---
+++ 6-semana/errors.py
@@ -0,0 +1,6 @@
+try:
+    n = input("ingresar N:\n")
+    m = 4
+    print(n/m)
+except:
+    print("upps, error")
