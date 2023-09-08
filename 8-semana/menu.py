556632fe6ca8f23f86022de5c211dade62934787
DiegoEstela <die.estela@gmail.com>
2023-09-08 15:04:28 +0200

    packages

---
+++ 8-semana/menu.py
@@ -0,0 +1,6 @@
+from packages.module1 import Persona
+from packages.module2 import llamando_al_modulo2
+
+persona1 = Persona("Diego", "Estela")
+
+llamando_al_modulo2()
---
+++ 8-semana/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mi_primer_paquete",
+    version='1.0',
+    author="Diego Estela",
+    description='Estamos creando el primer paquete distribuido',
+    author_email="die.estela@gmail.com",
+    packages=['packages']
+)
